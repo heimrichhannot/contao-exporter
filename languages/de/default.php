cd5084006d6590a1c2a8060121b5e2af545f290c
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-04-11 11:11:06 +0200

    added default locales

--- languages/de/default.php
+++ languages/de/default.php
@@ -1,4 +1,15 @@
 <?php
 
-$GLOBALS['TL_LANG']['MSC']['exporter']['noConfigFound'] = 'Für die gewählte Aktion wurde keine Exporter-Konfiguration gefunden. Legen Sie diese über den Menüpunkt "Exporter" im Backend an.';
-$GLOBALS['TL_LANG']['MSC']['exporter']['unformatted'] = ' (unformatiert)';
+$arrLang = &$GLOBALS['TL_LANG']['MSC'];
+
+/**
+ * Fields
+ */
+$arrLang['export'] = array('Exportieren (CSV)', 'Als CSV-Datei exportieren');
+$arrLang['export_xls'] = array('Exportieren (Excel)', 'Als Excel-Datei exportieren');
+
+/**
+ * Messages
+ */
+$arrLang['exporter']['noConfigFound'] = 'Für die gewählte Aktion wurde keine Exporter-Konfiguration gefunden. Legen Sie diese über den Menüpunkt "Exporter" im Backend an.';
+$arrLang['exporter']['unformatted'] = ' (unformatiert)';
