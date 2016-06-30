f0b5e41ee10e81cd5c77087d2c6e9c20aa1899d9
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-06-30 14:42:20 +0200

    added local

---
+++ languages/en/default.php
@@ -0,0 +1,15 @@
+<?php
+
+$arrLang = &$GLOBALS['TL_LANG']['MSC'];
+
+/**
+ * Fields
+ */
+$arrLang['export_csv'] = array('Export (CSV)', 'Export as CSV file');
+$arrLang['export_xls'] = array('Export (Excel)', 'Export as Excel file');
+
+/**
+ * Messages
+ */
+$arrLang['exporter']['noConfigFound'] = 'For the chosen action no export config has been found. Please create one by clicking on the backend menu entry "Exporter".';
+$arrLang['exporter']['unformatted'] = ' (unformatted)';
---
+++ languages/en/modules.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * Backend
+ */
+$GLOBALS['TL_LANG']['MOD']['exporter'] = array('Exporter', 'Export any entity using a global config.');
